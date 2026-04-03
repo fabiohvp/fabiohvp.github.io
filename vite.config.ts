a8d35913ea7144a7ab38dd88512fac89060d4596
Fabio Henriques <fabiohvp@hotmail.com>
2026-04-03 03:43:10 -0300

    fix attempt 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+	base: './',
+	plugins: [react(), tsconfigPaths()],
+	resolve: {
+		alias: {
+			'@': '/src',
+		},
+	},
 })
